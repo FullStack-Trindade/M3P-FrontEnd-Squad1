a77eadfdd4bb1e85c1801c7c081230a4466cb9f9
rodrigopieritz <rodrigopieritz@gmail.com>
2023-10-14 12:22:17 -0300

    criado routes  e página cadastro pacientes

---
+++ src/Pages/CadastroPaciente/CadastroPaciente.jsx
@@ -0,0 +1,14 @@
+//import * as Styled from "./CadastroPaciente.style";
+
+//Importar form de paciente
+
+export const PacientePage = () => {
+  const render = () => {
+    return (
+      <>
+        <h1>Formulário paciente aqui</h1>
+      </>
+    );
+  };
+  return render();
+};
