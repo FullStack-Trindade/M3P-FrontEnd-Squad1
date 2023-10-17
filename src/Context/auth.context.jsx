6bc2d325dc635d11df4f230d9460914ae21d9349
DaniSimoni <w.daniels@hotmail.com>
2023-10-17 18:57:19 -0300

    LoginPage com db.json LocalStorage aguardando o BackEnd e Auth

--- src/Context/auth.context.jsx
+++ src/Context/auth.context.jsx
@@ -1,1 +1,29 @@
-// será feito na segunda sprint
+import { createContext, useState } from "react";
+import PropTypes from 'prop-types';
+
+export const AuthContext = createContext({
+    auth: {
+        user: {},
+        isLogged: false,
+    },
+    setAuth: () => {},
+});
+
+export const AuthProvider = ({children}) => {
+    const [auth, setAuth] = useState({
+        user: {},
+        isLogged: false,
+    })
+    
+    return (
+        <AuthContext.Provider value={{auth, setAuth}}>
+            {children}
+        </AuthContext.Provider>
+
+
+    )
+}
+
+AuthProvider.propTypes = {
+    children: PropTypes.node,
+}
