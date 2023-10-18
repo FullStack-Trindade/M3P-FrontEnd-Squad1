20eee1ba3f7d21597af6fb39abe8043cb65e3e18
DaniSimoni <w.daniels@hotmail.com>
2023-10-18 19:59:44 -0300

    InputSearchPaciente, CardPaciente e Paciente.service

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import * as Styled from './HomePage.style'
 import { useContext, useEffect } from 'react';
 import { HeaderContext } from '../../Context/Header.context';
 import AreaEstatistica from '../../Components/AreaEstatísticas/AreaEstatisticas';
-/* import {InputSearch}  from '../../Components/InputSearch/InputSearch'; */
+import {InputSearch}  from '../../Components/InputSearchPaciente/InputSearchPaciente';
 import { AuthContext } from '../../Context/auth.context';
 import { Navigate } from 'react-router-dom';
 
@@ -27,7 +27,7 @@ export const HomePage = () => {
           <>
           <Styled.MainHome>
            { <AreaEstatistica/>}
-           {/*  <InputSearch/> */}
+           { <InputSearch/>}
 
           </Styled.MainHome>
           </>
