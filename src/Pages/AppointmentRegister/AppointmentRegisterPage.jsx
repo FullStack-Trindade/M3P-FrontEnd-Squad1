e93eed58134a95f0937538d67db9317a8f92e4f4
biachristie <beatrizchristie@yahoo.com.br>
2023-10-20 20:45:34 -0300

    added temp title to appointment register page

--- src/Pages/AppointmentRegister/AppointmentRegisterPage.jsx
+++ src/Pages/AppointmentRegister/AppointmentRegisterPage.jsx
@@ -1,16 +1,23 @@
 import * as Styled from './AppointmentRegisterPage.style';
+// import { useContext, useEffect } from 'react';
 
+// import { HeaderContext } from '../../Context/Header.context';
 import { FormAppointment } from '../../Components/FormAppointment/FormAppointment';
 
 export const AppointmentRegisterPage = () => {
+
+  // const { data, setData } = useContext(HeaderContext)
+
+  // useEffect(() => {
+  //   setData({ titulo: 'CADASTRO DE CONSULTA' })
+  // }, []);
 
   const render = () => {
       return (
         <>
           <Styled.RegisterArea>
-              <Styled.Title>
-                <FormAppointment />
-              </Styled.Title>
+            {/* <Styled.Title>{ data.titulo }</Styled.Title> */}
+            <FormAppointment />
           </Styled.RegisterArea>
         </>
       )
