bf1fad5f44667e4fe6d47678b509230f56cc8b66
biachristie <beatrizchristie@yahoo.com.br>
2023-10-20 18:02:22 -0300

    added update to appointment service

--- src/Service/Appointment.service.jsx
+++ src/Service/Appointment.service.jsx
@@ -34,8 +34,23 @@ const Show = (id) => {
     return fetchAppointment();
 }
 
+const Update = (id, data) => {
+    const fetchAppointment = async() => {
+        const response = await fetch(`${API_URL}/${ id }`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify( data )
+        })
+
+        return response
+    }
+
+    return fetchAppointment()
+}
+
 export const AppointmentService = {
     Get,
     Create,
-    Show
+    Show,
+    Update
 }
