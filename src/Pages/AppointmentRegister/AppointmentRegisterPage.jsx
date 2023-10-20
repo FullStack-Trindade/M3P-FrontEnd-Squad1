e90c874b690782628472a41c2dd2c63d4d766475
biachristie <beatrizchristie@yahoo.com.br>
2023-10-20 20:33:44 -0300

    added formAppointment to appointment register page

--- src/Pages/AppointmentRegister/AppointmentRegisterPage.jsx
+++ src/Pages/AppointmentRegister/AppointmentRegisterPage.jsx
@@ -1,9 +1,17 @@
+import * as Styled from './AppointmentRegisterPage.style';
+
+import { FormAppointment } from '../../Components/FormAppointment/FormAppointment';
+
 export const AppointmentRegisterPage = () => {
 
   const render = () => {
       return (
         <>
-          
+          <Styled.RegisterArea>
+              <Styled.Title>
+                <FormAppointment />
+              </Styled.Title>
+          </Styled.RegisterArea>
         </>
       )
   }
