e81fcca5c06c3fe3f83aa3b20d93d3316a255897
biachristie <beatrizchristie@yahoo.com.br>
2023-10-20 20:49:45 -0300

    added header provider to main

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 
+import { HeaderProvider } from './Context/Header.context.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+    <HeaderProvider>
+      <App />
+    </HeaderProvider>
+
   </React.StrictMode>,
 )
