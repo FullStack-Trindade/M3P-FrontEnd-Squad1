1d81328f1e3b06a9d7ecc121dc9b334fb4cc5286
biachristie <beatrizchristie@yahoo.com.br>
2023-10-20 16:06:08 -0300

    added label to input.style

--- src/Components/Form/InputComponent/Input.style.jsx
+++ src/Components/Form/InputComponent/Input.style.jsx
@@ -6,3 +6,16 @@ export const InputGroup = styled.div`
   align-items: flex-start;
   width: ${({ $width }) => ($width ? $width : '100%')};
 `;
+
+export const Label = styled.label`
+  display: flex;
+  align-items: flex-start;
+  gap: 0.625rem;
+  align-self: stretch;
+  color: ${({ $color }) => { return $color === 'danger' ? '#BE2E2E' : '#5281DC'}};
+  font-size: 0.75rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  width: 80%;
+`;
