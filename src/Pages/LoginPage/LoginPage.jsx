73eecc64ee1c615ddb6843f2acc992507e1856f2
rodrigopieritz <rodrigopieritz@gmail.com>
2023-10-23 21:21:15 -0300

    exportado menucomponent

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -21,6 +21,7 @@ export const LoginPage = () => {
           <Styled.DivCriarConta>
             <Styled.LabelCriarConta>Não possui uma conta?</Styled.LabelCriarConta>
             <Styled.ButtonHeaderLogin onClick={() => alert('Entre em contato com o Administrador do Serviço e solicite o seu cadastro')}>Criar Conta</Styled.ButtonHeaderLogin>
+            
           </Styled.DivCriarConta>
 
           <Styled.Login>
