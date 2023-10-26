c24b6f263085428e78c24526c2dad3c314adbb3f
biachristie <beatrizchristie@yahoo.com.br>
2023-10-26 09:37:16 -0300

    added search input to inputSearchAppointment

--- src/Components/InputSearchAppointment/InputSearchAppointment.jsx
+++ src/Components/InputSearchAppointment/InputSearchAppointment.jsx
@@ -1,12 +1,31 @@
 import * as Styled from './InputSearchAppointment.style';
 
-export const InputSearchAppointment = () => {
+import { InputComponent } from '../Form/InputComponent/InputComponent';
 
+export const InputSearchAppointment = () => {
+    
     return (
         <>
             <Styled.InputContainer>
 
                 <h4>Encontre o paciente:</h4>
+
+                <Styled.SearchInput>
+                    <InputComponent 
+                        $width={'100%'}
+                        id='namePatient'
+                        type='text'
+                        placeholder='Digite o nome do paciente'
+                        name='namePatient'
+                    />
+
+                    <button 
+                        className="button" 
+                        type='submit'
+                    >
+                        Buscar
+                    </button>
+                </Styled.SearchInput>
 
             </Styled.InputContainer>
         </>
