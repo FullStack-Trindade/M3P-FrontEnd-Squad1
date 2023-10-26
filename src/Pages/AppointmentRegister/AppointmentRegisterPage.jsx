3e4c6edfd7f8f4299b1dca8449a2f29a1581cf8f
biachristie <beatrizchristie@yahoo.com.br>
2023-10-26 16:14:07 -0300

    removed title from AppointmentRegisterPage

--- src/Pages/AppointmentRegister/AppointmentRegisterPage.jsx
+++ src/Pages/AppointmentRegister/AppointmentRegisterPage.jsx
@@ -1,24 +1,23 @@
 import * as Styled from './AppointmentRegisterPage.style';
-// import { useContext, useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 // import { Navigate } from 'react-router-dom';
 
-// import { HeaderContext } from '../../Context/Header.context';
+import { HeaderContext } from '../../Context/Header.context';
 import { InputSearchAppointment } from '../../Components/InputSearchAppointment/InputSearchAppointment';
 
 export const AppointmentRegisterPage = () => {
   // const isLogged = JSON.parse(localStorage.getItem('isLogged'));
 
-  // const { data, setData } = useContext(HeaderContext)
+  const { setData } = useContext(HeaderContext)
 
-  // useEffect(() => {
-  //   setData({ titulo: 'CADASTRO DE CONSULTA' })
-  // }, []);
+  useEffect(() => {
+    setData({ titulo: 'CADASTRO DE CONSULTA' })
+  }, []);
 
   const render = () => {
       return (
         <>
           <Styled.RegisterArea>
-            {/* <Styled.Title>{ data.titulo }</Styled.Title> */}
             <InputSearchAppointment/>
           </Styled.RegisterArea>
         </>
