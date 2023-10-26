e2c124fa43e3c323d8cdd35cc836dc0cf01ebc25
biachristie <beatrizchristie@yahoo.com.br>
2023-10-26 16:12:16 -0300

    added GlobalStyle to main

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 
+import { GlobalStyle } from './Layout/Global.style'
 import { HeaderProvider } from './Context/Header.context.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyle/>
 
     <HeaderProvider>
       <App />
