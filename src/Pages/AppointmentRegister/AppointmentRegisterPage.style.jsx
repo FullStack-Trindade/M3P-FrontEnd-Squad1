b52470e38b9bf6eb03124a6ed3e5bec73a85b67a
biachristie <beatrizchristie@yahoo.com.br>
2023-10-26 16:13:45 -0300

    removed title from AppointmentRegisterPage.style

--- src/Pages/AppointmentRegister/AppointmentRegisterPage.style.jsx
+++ src/Pages/AppointmentRegister/AppointmentRegisterPage.style.jsx
@@ -7,15 +7,3 @@ export const RegisterArea = styled.div`
   height: 100%;
   color: white;
 `;
-
-export const Title = styled.h1`
-  display: flex;
-  align-items: column;
-  color: gray;
-  font-size: 2rem;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  width: 100%;
-  margin: 0 5% -5% 5%;
-`;
