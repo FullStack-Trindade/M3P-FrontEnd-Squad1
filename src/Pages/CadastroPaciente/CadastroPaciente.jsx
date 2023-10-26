523f6c69d05592e907da49ea5ef3ee6acc8ac617
rodrigopieritz <rodrigopieritz@gmail.com>
2023-10-26 15:38:37 -0300

    atualizados os campos para área de cadastro

--- src/Pages/CadastroPaciente/CadastroPaciente.jsx
+++ src/Pages/CadastroPaciente/CadastroPaciente.jsx
@@ -1,15 +1,25 @@
 import * as Styled from "./CadastroPaciente.style";
+import { useContext, useEffect } from "react";
+import { HeaderContext } from "../../Context/Header.context";
 
 import { FormPaciente } from "../../Components/FormPaciente/FormPaciente";
 
-
 export const PacientePage = () => {
+  const { setData } = useContext(HeaderContext);
+  useEffect(() => {
+    setData({
+      titulo: "CADASTRO DE PACIENTES",
+    });
+  }, []);
+
   const render = () => {
     return (
-      <>
-        <h1>CADASTRO DE PACIENTES</h1>
-        <FormPaciente/>
-      </>
+      <Styled.AreaCadastro>
+        <Styled.Title>Preencha os campos para cadastrar</Styled.Title>
+        <Styled.AreaPaciente>
+          <FormPaciente />
+        </Styled.AreaPaciente>
+      </Styled.AreaCadastro>
     );
   };
   return render();
