7e5f39abd04493bfa7280ccf68fee325123439ab
biachristie <beatrizchristie@yahoo.com.br>
2023-10-26 16:11:53 -0300

    added Layout to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import { Layout } from './Layout/Layout'
 import { AppointmentRegisterPage } from "./Pages/AppointmentRegister/AppointmentRegisterPage.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/consulta" element={ <AppointmentRegisterPage /> } />
-
+        <Route path='/' element={<Layout/>}>
+          <Route path="/consulta" element={ <AppointmentRegisterPage /> } />
+        </Route>
         <Route path='*' element={ <><p>Essa página não existe</p></> }/>
       </Routes>
     </Router>
