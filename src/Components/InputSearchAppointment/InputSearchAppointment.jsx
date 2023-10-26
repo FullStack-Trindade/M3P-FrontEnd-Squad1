c0f4ff1a15cd04f957d56770bb76dc936e2a23cc
biachristie <beatrizchristie@yahoo.com.br>
2023-10-26 09:33:33 -0300

    added input container to inputSearchAppointment

--- src/Components/InputSearchAppointment/InputSearchAppointment.jsx
+++ src/Components/InputSearchAppointment/InputSearchAppointment.jsx
@@ -1,7 +1,14 @@
+import * as Styled from './InputSearchAppointment.style';
+
 export const InputSearchAppointment = () => {
+
     return (
         <>
+            <Styled.InputContainer>
 
+                <h4>Encontre o paciente:</h4>
+
+            </Styled.InputContainer>
         </>
     )
 }
