537e2c21a5fca9e42f06e85c4598b73bd2064bd1
rodrigopieritz <rodrigopieritz@gmail.com>
2023-10-30 19:11:13 -0300

    ajustados os services

--- src/Service/Exam.service.jsx
+++ src/Service/Exam.service.jsx
@@ -1,43 +1,43 @@
 const fetchExam = async (url, options) => {
-    try {
-      const response = await fetch(url, options);
-     
-      if (!response.ok) {
-        throw new Error(`Erro na requisição: ${response.status}`);
-      }
-  
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("Erro na chamada da API:", error);
-      throw error;
+  try {
+    const response = await fetch(url, options);
+    console.log(response.status);
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status}`);
     }
-  };
-  
-  export const ExamService = {
-    Create: (data) => {
-       return fetchExam("http://localhost:3000/api/exames", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-    },
-    Get: () => {
-      return fetchExam("http://localhost:3000/api/exames");
-    },
-    
-      Update: (id, data) => {
-      return fetchExam(`http://localhost:3000/api/exames/${id}`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
-      });
-    },
-    Delete: (id) => {
-      return fetchExam(`http://localhost:3000/api/exames/${id}`, {
-        method: "DELETE",
-      });
-    },
-  };
+
+    const data = response;
+    return data;
+  } catch (error) {
+    console.error("Erro na chamada da API:", error);
+    throw error;
+  }
+};
+
+export const ExamService = {
+  Create: (data) => {
+    return fetchExam("http://localhost:3000/api/exames", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  },
+  Get: () => {
+    return fetchExam("http://localhost:3000/api/exames");
+  },
+
+  Update: (id, data) => {
+    return fetchExam(`http://localhost:3000/api/exames/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+  },
+  Delete: (id) => {
+    return fetchExam(`http://localhost:3000/api/exames/${id}`, {
+      method: "DELETE",
+    });
+  },
+};
