640ea140c2f85bc5bb320da6378d03f47f395038
biachristie <beatrizchristie@yahoo.com.br>
2023-10-30 21:16:46 -0300

    added container area to medicalRecordPage style

---
+++ src/Pages/MedicalRecords/MedicalRecordsPage.style.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ContainerArea = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    color: white;
+    margin: 0 auto;
+`;
