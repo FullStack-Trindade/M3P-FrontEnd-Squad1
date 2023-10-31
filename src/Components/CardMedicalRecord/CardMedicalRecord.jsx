9f66a80ba6a61a0cac79e8b0e8abff8d21441e82
biachristie <beatrizchristie@yahoo.com.br>
2023-10-30 21:38:35 -0300

    added styled tags to cardMedicalRecord

--- src/Components/CardMedicalRecord/CardMedicalRecord.jsx
+++ src/Components/CardMedicalRecord/CardMedicalRecord.jsx
@@ -1,8 +1,16 @@
+import * as Styled from './CardMedicalRecord.style';
+
 export const CardMedicalRecord = ({ patient, user }) => {
 
     return (
         <>
-            
+            <Styled.CardContainer>
+                <Styled.RenderCard>
+                    <Styled.PatientData>
+
+                    </Styled.PatientData>
+                </Styled.RenderCard>
+            </Styled.CardContainer>
         </>
     );
 };
