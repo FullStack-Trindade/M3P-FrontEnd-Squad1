372ffaaae9637e14a0b90664ba5bdbde55b00fe4
biachristie <beatrizchristie@yahoo.com.br>
2023-10-30 21:46:45 -0300

    added medicalRecordsPage route to app

--- App.jsx
+++ App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+import { Layout } from './src/Layout/Layout'
 import { LoginPage } from './src/Pages/LoginPage/LoginPage'
 import { HomePage } from './src/Pages/HomePage/HomePage.jsx'
-import { Layout } from './src/Layout/Layout'
 import { CadastroExamePage } from './src/Pages/CadastroExame/CadastroExamePage.jsx'
+import { MedicalRecordsPage } from './src/Pages/MedicalRecords/MedicalRecordsPage'
 /* import { PacientePage } from './assets/Pages/CadastroPaciente/CadastroPaciente.jsx'
 import { ConsultaPage } from './assets/Pages/CadastroConsulta/CadastroConsulta.jsx'
-import { ListaProntuarioPage } from './assets/Pages/ListaProntuario/ListaProntuario.jsx'
 import { ProntuarioPage } from './assets/Pages/ProntuarioPage/Prontuario.jsx' */
 
 function App() {
@@ -14,18 +14,19 @@ function App() {
 
     <Router>
       <Routes>
-          <Route path='/login' element={<LoginPage/>}/>
-          <Route path='/' element={<Layout/>}>
-              <Route path='/' element={<HomePage/>}/>
-              <Route path='/exame' element={<CadastroExamePage/>}/>
+        <Route path='/login' element={ <LoginPage/> }/>
+
+        <Route path='/' element={ <Layout/> }>
+          <Route path='/' element={ <HomePage/> }/>
+          <Route path='/exame' element={ <CadastroExamePage/> }/>
+          <Route path='/listaProntuarios' element={ <MedicalRecordsPage/> }/>
 {/*               <Route path='/paciente' element={<PacientePage/>}/>
-              <Route path='/consulta' element={<ConsultaPage/>}/>
-              <Route path='/listaProntuarios' element={<ListaProntuarioPage/>}/>
-              <Route path='/prontuarios/:id' element={<ProntuarioPage/>}/> */}
-          </Route>
-          <Route path='*' element={<><p>Essa página não existe</p></>}/>
-          
-       </Routes>
+          <Route path='/consulta' element={<ConsultaPage/>}/>
+          <Route path='/prontuarios/:id' element={<ProntuarioPage/>}/> */}
+        </Route>
+
+        <Route path='*' element={ <><p>Essa página não existe</p></> }/>
+      </Routes>
     </Router>
   )
 }
