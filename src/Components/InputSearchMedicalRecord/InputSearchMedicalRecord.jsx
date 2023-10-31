e9a2ab86ca9b5ef7fd3f8a2acf3418e49a715a12
biachristie <beatrizchristie@yahoo.com.br>
2023-10-30 21:22:59 -0300

    added styled input container to inputSearchMedicalRecord

--- src/Components/InputSearchMedicalRecord/InputSearchMedicalRecord.jsx
+++ src/Components/InputSearchMedicalRecord/InputSearchMedicalRecord.jsx
@@ -1,8 +1,14 @@
+import * as Styled from './InputSearchMedicalRecord.style';
+
 export const InputSearchMedicalRecord = () => {
 
     return (
         <>
-            
+            <Styled.InputContainer>
+
+                <h4>Encontre o paciente</h4>
+
+            </Styled.InputContainer>
         </>
     )
 }
