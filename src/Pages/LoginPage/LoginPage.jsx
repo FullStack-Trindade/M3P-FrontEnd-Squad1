ba90b24087b54ff76c46fa97010fe10be93007bd
biachristie <beatrizchristie@yahoo.com.br>
2023-10-31 11:54:11 -0300

    modified conditional redirection at loginPage

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -5,10 +5,27 @@ import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../../Context/auth.context';
 import { FormLoginComponent } from "../../Components/LoginForm/LoginFormComponent";
 import imagemLogin from '../../Assets/LogoGenerica.jpg';
+import { useEffect } from 'react';
+import { AuthService } from '../../Service/Auth.service';
 
 export const LoginPage = () => {
-    const { tokenUser } = useContext(AuthContext);
-    const localToken = (JSON.parse(localStorage.getItem('token')));
+    const { tokenUser, setTokenUser } = useContext(AuthContext);
+    const localToken = JSON.parse(localStorage.getItem('token'));
+
+    useEffect(() => { 
+        if (localToken !== null) {
+            fetchAuth() 
+        }
+    }, [localToken]);
+
+    const fetchAuth = async() => {
+        const authToken = await AuthService.Get();
+        const tokenExists = authToken.filter(auth => auth.token_user === localToken);
+
+        if (tokenExists.length === 0) { return }
+        
+        setTokenUser(tokenExists[0]?.token_user);
+    }
 
     const render = () => {
         return (
