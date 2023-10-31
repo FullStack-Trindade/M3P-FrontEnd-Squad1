44d39eb1c3b6f0a1c0b6e6e53369b50b78662cd5
biachristie <beatrizchristie@yahoo.com.br>
2023-10-31 09:57:47 -0300

    added page redirection conditional to login page

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,35 +1,44 @@
-import { FormLoginComponent } from "../../Components/LoginForm/LoginFormComponent";
-/* import { useNavigate } from 'react-router-dom'; */
-
-
 import * as Styled from './LoginPage.style';
+import { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
 
+import { AuthContext } from '../../Context/auth.context';
+import { FormLoginComponent } from "../../Components/LoginForm/LoginFormComponent";
 import imagemLogin from '../../Assets/LogoGenerica.jpg';
 
-
-
 export const LoginPage = () => {
+    const { tokenUser } = useContext(AuthContext);
+    const localToken = (JSON.parse(localStorage.getItem('token')));
 
+    const render = () => {
+        return (
+            <>
+                <Styled.ContainerLogin>
+        
+                    <Styled.ImageLogin src={ imagemLogin } />
+        
+                    <Styled.BarraCentral/>
+        
+                    <Styled.DivCriarConta>
+                        <Styled.LabelCriarConta>Não possui uma conta?</Styled.LabelCriarConta>
+                        <Styled.ButtonHeaderLogin 
+                            onClick={ () => alert('Entre em contato com o Administrador do Serviço e solicite o seu cadastro') }
+                        >
+                            Criar Conta
+                        </Styled.ButtonHeaderLogin>
+                    </Styled.DivCriarConta>
+        
+                    <Styled.Login>
+                        <FormLoginComponent/>
+                    </Styled.Login>
+        
+                </Styled.ContainerLogin>
+            </>
+        )
+    }
 
-  return (
-    <>
+    return !!tokenUser && (tokenUser === localToken) ? <Navigate to='/' /> : render();
 
-      <Styled.ContainerLogin>
-          <Styled.ImageLogin src={imagemLogin} />
-          <Styled.BarraCentral/>
-
-          <Styled.DivCriarConta>
-            <Styled.LabelCriarConta>Não possui uma conta?</Styled.LabelCriarConta>
-            <Styled.ButtonHeaderLogin onClick={() => alert('Entre em contato com o Administrador do Serviço e solicite o seu cadastro')}>Criar Conta</Styled.ButtonHeaderLogin>
-          </Styled.DivCriarConta>
-
-          <Styled.Login>
-            <FormLoginComponent/>
-          </Styled.Login>
-
-      </Styled.ContainerLogin>
-    </>
-  )
 }
 
 
