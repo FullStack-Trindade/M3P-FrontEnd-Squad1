30a94064bfc0d42d1ae021acacb7d536c5133f3f
biachristie <beatrizchristie@yahoo.com.br>
2023-10-31 09:43:01 -0300

    added idUser, tokenUser and idType to authContext

--- src/Context/auth.context.jsx
+++ src/Context/auth.context.jsx
@@ -1,26 +1,26 @@
-import { createContext, useState } from "react";
 import PropTypes from 'prop-types';
+import { createContext, useState } from "react";
 
 export const AuthContext = createContext({
-    auth: {
-        user: {},
-        isLogged: false,
-    },
-    setAuth: () => {},
+    idUser: null,
+    setIdUser: () => {},
+    tokenUser: null,
+    setTokenUser: () => {},
+    idType: null,
+    setIdType: () => {}
 });
 
-export const AuthProvider = ({children}) => {
-    const [auth, setAuth] = useState({
-        user: {},
-        isLogged: false,
-    })
+export const AuthProvider = ({ children }) => {
+    const [idUser, setIdUser] = useState();
+    const [tokenUser, setTokenUser] = useState();
+    const [idType, setIdType] = useState();
     
     return (
-        <AuthContext.Provider value={{auth, setAuth}}>
-            {children}
+        <AuthContext.Provider 
+            value={{ idUser, setIdUser, tokenUser, setTokenUser, idType, setIdType }}
+        >
+            { children }
         </AuthContext.Provider>
-
-
     )
 }
 
