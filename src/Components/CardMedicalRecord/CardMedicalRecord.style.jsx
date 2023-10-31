2f2e8343771bdc29aebaa9222b670dd076496475
biachristie <beatrizchristie@yahoo.com.br>
2023-10-30 21:35:59 -0300

    added render card to cardMedicalRecord style

--- src/Components/CardMedicalRecord/CardMedicalRecord.style.jsx
+++ src/Components/CardMedicalRecord/CardMedicalRecord.style.jsx
@@ -5,3 +5,11 @@ export const CardContainer = styled.div`
     width: 100%;
     margin: 4vw 0 0 0;
 `
+
+export const RenderCard = styled.div`
+    display: flex;
+    width: 100%;
+    margin: auto;
+    border-radius: 5px;
+    box-shadow: 3px 3px 3px gray;
+`
