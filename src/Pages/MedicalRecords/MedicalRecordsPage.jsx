21c461c86e0445ab18a8c5341dcd4679d6832b93
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-01 17:17:37 -0300

    added token conditional redirection to medicalRecordsPage

--- src/Pages/MedicalRecords/MedicalRecordsPage.jsx
+++ src/Pages/MedicalRecords/MedicalRecordsPage.jsx
@@ -1,12 +1,29 @@
 import * as Styled from './MedicalRecordsPage.style'
 import { useContext, useEffect } from 'react';
-// import { Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
+import { AuthContext } from '../../Context/auth.context';
 import { HeaderContext } from '../../Context/Header.context';
 import { InputSearchMedicalRecord } from '../../Components/InputSearchMedicalRecord/InputSearchMedicalRecord';
 
 export const MedicalRecordsPage = () => {
-    // const isLogged = JSON.parse(localStorage.getItem('isLogged'));
+    const { tokenUser, setTokenUser } = useContext(AuthContext);
+    const localToken = JSON.parse(localStorage.getItem('token'));
+
+    useEffect(() => { 
+        if (localToken !== null) {
+            fetchAuth() 
+        }
+    }, [localToken]);
+
+    const fetchAuth = async() => {
+        const authToken = await AuthService.Get();
+        const tokenExists = authToken.filter(auth => auth.token_user === localToken);
+
+        if (tokenExists.length === 0) { return }
+        
+        setTokenUser(tokenExists[0]?.token_user);
+    }
 
     const render = () => {
 
@@ -27,7 +44,5 @@ export const MedicalRecordsPage = () => {
         )
     }
 
-    // return isLogged ? render() : <Navigate to='/login' />
-
-    return render();
+    return !!tokenUser && (tokenUser === localToken) ? render() : <Navigate to='/login'/>;
 }
