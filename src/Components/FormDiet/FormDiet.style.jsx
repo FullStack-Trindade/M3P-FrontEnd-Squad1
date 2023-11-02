1df4fd0f80c6899b0513be1fe0d86c1298cfebf4
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-02 14:00:56 -0300

    added form to formDiet style

---
+++ src/Components/FormDiet/FormDiet.style.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+    display: inline-flex;
+    padding: 2rem;
+    width: 100%;
+    height: auto;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 5%;
+    gap: 1rem;
+    border-radius: 0.625rem;
+    background: #FFF;
+    box-shadow: 5px 5px 5px gray;
+`;
