ec0fb86875ce707393234954948055ad9d688176
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-02 13:26:13 -0300

    added register area to dietRegisterPage style

---
+++ src/Pages/DietRegister/DietRegisterPage.style.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const RegisterArea = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    color: white;
+`;
