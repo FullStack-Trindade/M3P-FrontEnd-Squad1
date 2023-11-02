5eb931359698f4ae5faaa664530c18f123f563db
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-02 14:17:21 -0300

    added title to formDiet

--- src/Components/FormDiet/FormDiet.jsx
+++ src/Components/FormDiet/FormDiet.jsx
@@ -1,7 +1,22 @@
 export const FormDiet = ({ patientId }) => {
     return (
         <>
-        
+            
+            <Styled.Form>
+
+                <Styled.Header>
+
+                    <Styled.Title>
+                        {
+                            patientName
+                            ? `Dieta de ${ patientName }`
+                            : 'Formulário de Dieta' 
+                        }
+                    </Styled.Title>
+
+                </Styled.Header>
+            </Styled.Form>
+
         </>
     )
 }
