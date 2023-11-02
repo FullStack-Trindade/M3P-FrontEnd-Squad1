97963e5eedfa53b0e668c1ad97cc5d83add8f1dc
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-02 13:46:26 -0300

    added input container to inputSearchDiet

--- src/Components/InputSearchDiet/InputSearchDiet.jsx
+++ src/Components/InputSearchDiet/InputSearchDiet.jsx
@@ -1,7 +1,13 @@
+import * as Styled from './InputSearchDiet.style';
+
 export const InputSearchDiet = () => {
     return (
         <>
-        
+            <Styled.InputContainer>
+
+                <h4>Encontre o paciente</h4>
+
+            </Styled.InputContainer>
         </>
     )
 }
