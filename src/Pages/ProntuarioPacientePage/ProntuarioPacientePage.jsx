25cf9628c24c5f7e8072f5613b597be2c6e294f7
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-02 12:11:49 -0300

    fetch patient record at prontuarioPacientePage

--- src/Pages/ProntuarioPacientePage/ProntuarioPacientePage.jsx
+++ src/Pages/ProntuarioPacientePage/ProntuarioPacientePage.jsx
@@ -1,89 +1,79 @@
 import * as Styled from './ProntuarioPacientePage.style'
-
 import { useContext, useEffect, useState } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import dayjs from 'dayjs';
+
 import { HeaderContext } from '../../Context/Header.context';
-
-import { useLocation } from 'react-router-dom';
-
-import { PacienteService } from '../../Service/Paciente.service'; 
-/* import { ConsultaService } from '../../../../src/Service/User/Consulta.service';
-import { ExameService } from '../../../../src/Service/User/Exame.service'; */
+import { PatientRecordService } from '../../Service/PatientRecord.service';
 
 /* import CardConsulta from '../../Components/CardConsulta/CardConsulta';
-
 import CardExame from '../../Components/CardExame/CardExame'; */
 
 export const ProntuarioPage = () => {
+  const { pathname } = useLocation();
+  const location = pathname.split('/');
+  const patientId = location[location.length - 1];
 
+  const { setData } = useContext(HeaderContext);
 
-  const [paciente, setPaciente] = useState()
-  const [consulta, setConsulta] = useState()
-  const [exame, setExame] = useState()
-  
-
-  const { setData } = useContext(HeaderContext)
-  const {pathname} = useLocation()
+  const [patientsRecordList, setPatientsRecordList] = useState([]);
 
   useEffect(() => {
-    setData({       
-      titulo: 'LISTAGEM DE PRONTUÁRIOS'}) 
-      const location = pathname.split('/')
-      const pacienteId = location[location.length - 1]
-      const consultaId = location[location.consulta]
-      const exameId = location[location.exame]
-      
-      const asyncFn = async () => {
-        await PacienteService.Show(pacienteId).then(async (response)  => {
-          setPaciente(response)
-          await ConsultaService.Get().then(res => setConsulta(res.filter(cc => cc.pacienteId === response.id)))
-          await ExameService.Get().then(res => setExame(res.filter(e => e.pacienteId === response.id)))
-        })
+    setData({ titulo: 'LISTAGEM DE PRONTUÁRIOS' });
 
-        
-      }
-      asyncFn()
-    }, []);
+    if (patientId !== null) {
+      fetchPatientsRecord();
+    }
+  }, []);
+    
+    const fetchPatientsRecord = async() => {
+      await PatientRecordService.Show().then(result  => setPatientsRecordList(result));
+    }
 
-  
+    const [patientRecord, setPatientRecord] = useState([]);
+
+    useEffect(() => {
+      const filteredPatient = patientsRecordList.filter(value => value.id.toString().includes(patientId))
+      setPatientRecord(filteredPatient);
+  }, [patientsRecordList, patientId])
 
     const render = () => {
         return (
+          <>
+              <Styled.Prontuario>
+                  <Styled.HeaderProntuario>
+                      <Styled.Title>{ patientRecord[0]?.user.name }</Styled.Title>
 
-          <>          
-          
-            <Styled.Prontuario>
-            <Styled.HeaderProntuario>
-                <Styled.Title>{paciente?.nome}</Styled.Title>
-                <Styled.Label>Convênio: {paciente?.convenio}</Styled.Label>
-                <Styled.Label>Nacimento: {paciente?.nasc}</Styled.Label>
-                <Styled.Label>Contato: {paciente?.tel}</Styled.Label>
-            </Styled.HeaderProntuario>
+                      <Styled.PatientData>
+                        <Styled.List>Nascimento: { dayjs(patientRecord[0]?.birth).format('DD/MM/YYYY') }</Styled.List>
+                        <Styled.List>Contato de Emergência: { patientRecord[0]?.emergencyContact }</Styled.List>
+                        <Styled.List>Convênio: { patientRecord[0]?.healthInsurance }</Styled.List>
+                        <Styled.List>Número do Convênio: { patientRecord[0]?.insuranceNumber }</Styled.List>
+                        <Styled.List>Validade do Convênio: { dayjs(patientRecord[0]?.insuranceVality).format('MM/YYYY') }</Styled.List>
+                      </Styled.PatientData>
 
-            <Styled.CorpoProntuario>
+                      <Styled.PatientData>
+                        <Styled.List>{ patientRecord[0]?.adress_id.street }, {patientRecord[0]?.adress_id.number}{' - ' + patientRecord[0]?.adress_id.complement }</Styled.List>
+                        <Styled.List>{ patientRecord[0]?.adress_id.cep.substring(0, 5).concat('-', patientRecord[0]?.adress_id.cep.substring(5, 8)) }</Styled.List>
+                        <Styled.List>{ patientRecord[0]?.adress_id.reference }</Styled.List>
+                      </Styled.PatientData>
+                  </Styled.HeaderProntuario>
 
-            <Styled.SubTitle><span>1</span>Consulta</Styled.SubTitle>
+                  <Styled.CorpoProntuario>
+                      <Styled.SubTitle><span>1</span>Consulta</Styled.SubTitle>
+                      <Styled.RenderResultados>
+                          {/* { patientRecord[0]?.appointments.map(appointment => <CardConsulta consulta={appointment} key={appointment.id} />) } */}
+                      </Styled.RenderResultados>
 
-              <Styled.RenderResultados>
-                  {consulta && consulta.map(consulta => <CardConsulta consulta={consulta} key={consulta.id} />)}
-              </Styled.RenderResultados>
-
-            <Styled.SubTitle><span>2</span>Exame</Styled.SubTitle>
-
-              <Styled.RenderResultados>
-                  {exame && exame.map(exame => <CardExame exame={exame} key={exame.id} />)}       
-              </Styled.RenderResultados>
-            </Styled.CorpoProntuario>
-          </Styled.Prontuario>
-
+                      <Styled.SubTitle><span>2</span>Exame</Styled.SubTitle>
+                      <Styled.RenderResultados>
+                          {/* { patientRecord[0].exams.map(exam => <CardExame exame={exam} key={exam.id} />) } */}
+                      </Styled.RenderResultados>
+                  </Styled.CorpoProntuario>
+              </Styled.Prontuario>
           </>
       )
     }
 
-    return render()
-    
+    return render();
   }
-  
-
-
-  
-
