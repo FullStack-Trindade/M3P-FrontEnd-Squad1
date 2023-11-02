f0ce855eb27399a12138c85695b75ffd5783f54c
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-02 13:36:44 -0300

    added page conditional rendering to dietRegisterPage

--- src/Pages/DietRegister/DietRegisterPage.jsx
+++ src/Pages/DietRegister/DietRegisterPage.jsx
@@ -1,9 +1,28 @@
 import * as Styled from './DietRegisterPage.style';
 import { useContext, useEffect } from 'react';
+// import { Navigate } from 'react-router-dom';
 
+// import { AuthContext } from '../../Context/auth.context';
 import { HeaderContext } from '../../Context/Header.context';
 
 export const DietRegisterPage = () => {
+    // const { tokenUser, setTokenUser } = useContext(AuthContext);
+    // const localToken = JSON.parse(localStorage.getItem('token'));
+
+    // useEffect(() => { 
+    //     if (localToken !== null) {
+    //         fetchAuth() 
+    //     }
+    // }, [localToken]);
+
+    // const fetchAuth = async() => {
+    //     const authToken = await AuthService.Get();
+    //     const tokenExists = authToken.filter(auth => auth.token_user === localToken);
+
+    //     if (tokenExists.length === 0) { return }
+        
+    //     setTokenUser(tokenExists[0]?.token_user);
+    // }
 
     const { setData } = useContext(HeaderContext)
 
@@ -23,5 +42,6 @@ export const DietRegisterPage = () => {
         )
     }
 
+    // return !!tokenUser && (tokenUser === localToken) ? render() : <Navigate to='/login'/>;
     return render();
 }
