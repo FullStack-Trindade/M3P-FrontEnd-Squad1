85cd447ef9aa5547c320ae594ce58bb492aade5a
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-03 00:39:29 -0300

    bugfix: changed page redirection to end redirect bug at home and medical records pages

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import * as Styled from './HomePage.style'
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 
 import { AuthContext } from '../../Context/auth.context';
@@ -13,9 +13,10 @@ import { InputPatientSearchAtHome } from '../../Components/InputPatientSearchAtH
  */
 
 export const HomePage = () => {
-  
   const { tokenUser, setTokenUser } = useContext(AuthContext);
   const localToken = JSON.parse(localStorage.getItem('token'));
+
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => { 
       if (localToken !== null) {
@@ -24,14 +25,16 @@ export const HomePage = () => {
   }, [localToken]);
 
   const fetchAuth = async() => {
-      const authToken = await AuthService.Get();
-      const tokenExists = authToken.filter(auth => auth.token_user === localToken);
+    const authToken = await AuthService.Get();
+    const tokenExists = await authToken.filter(auth => auth.token_user === localToken);
 
-      if (tokenExists.length === 0) { return }
-      
-      setTokenUser(tokenExists[0]?.token_user);
+      if (tokenExists.length > 0) { 
+        setTokenUser(tokenExists[0]?.token_user);
+      }
+
+      setLoading(false);
   }
-  
+
   const { setData } = useContext(HeaderContext)
   
   useEffect(() => {
@@ -43,16 +46,15 @@ export const HomePage = () => {
         return (
           <>
           <Styled.MainHome>
-           { <AreaEstatistica/>}
+            { <AreaEstatistica/>}
           </Styled.MainHome>
           </>
       )
     }
 
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
     return !!tokenUser && (tokenUser === localToken) ? render() : <Navigate to='/login'/>;
-  }
-  
-
-
-  
-
+}
