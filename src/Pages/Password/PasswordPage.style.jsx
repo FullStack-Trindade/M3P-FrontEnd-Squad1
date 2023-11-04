4f14878cfde2089af6b0ac50b2029b0ec0f042b4
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-04 16:01:28 -0300

    added container password to passwordPage style

---
+++ src/Pages/Password/PasswordPage.style.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ContainerPassword = styled.div`
+    display: flex;
+    flex-direction: space-around;
+    align-items: center;
+    margin: 0 auto !important;
+    width: 100%;
+    height: auto;
+`;
