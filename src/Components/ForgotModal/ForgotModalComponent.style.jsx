2dec9be2b519cbdc157a08de42f9984edef1d0f5
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-04 16:20:26 -0300

    added modal title to forgotModalComponent style

--- src/Components/ForgotModal/ForgotModalComponent.style.jsx
+++ src/Components/ForgotModal/ForgotModalComponent.style.jsx
@@ -18,3 +18,15 @@ export const ModalContainer = styled.div`
     background-color: #fff;
     color: var(--color-porcelain);
 `;
+
+export const ModalTitle = styled.span`
+    display: inline-block;
+    width: 100%;
+    color:  #5281DC;
+    font-size: 2rem;
+    font-weight: 700;
+    text-align: center;
+    border-bottom: 1px solid var(--color-porcelain);
+    padding: 10px 0;
+    margin: 20px 0;
+`;
