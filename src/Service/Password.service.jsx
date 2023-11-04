5e35b08bab08773ee78d9dc8aefa2a43f25879f3
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-04 16:00:12 -0300

    added create token to password service

---
+++ src/Service/Password.service.jsx
@@ -0,0 +1,19 @@
+const API_URL = `http://localhost:${import.meta.env.VITE_SERVER_PORT}/api`;
+
+const CreateToken = (data) => {
+    const fetchPassword = async() => {
+        const response = await fetch(`${API_URL}/requisitarSenha`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify( data )
+        })
+
+        return response
+    }
+
+    return fetchPassword()
+}
+
+export const PasswordService = {
+    CreateToken
+}
