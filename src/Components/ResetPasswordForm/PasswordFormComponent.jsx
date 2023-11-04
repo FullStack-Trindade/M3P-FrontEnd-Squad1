7b30e9196f80b3bf90d14f4cad19b632cad5c043
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-04 16:16:45 -0300

    added submitForm to  passwordFormComponent

--- src/Components/ResetPasswordForm/PasswordFormComponent.jsx
+++ src/Components/ResetPasswordForm/PasswordFormComponent.jsx
@@ -1,14 +1,49 @@
 import * as Styled from './PasswordFormComponent.style';
 
 import { InputComponent } from '../FormPaciente/InputComponent/InputComponent';
+import { PasswordService } from '../../Service/Password.service';
 
 export const PasswordFormComponent = () => {
     let params = new URL(document.location).searchParams;
     const passwordToken = params.get('token');
     const userId = params.get('id');
     
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },  
+    } = useForm();
+
+    const submitForm = async (submitData) => {
+        const { password, confirmPassword } = submitData;
+
+        if(!confirmPassword || !password) {
+            return alert('Campos de senha são obrigatórios.');
+        }
+
+        const submitPasswordData = { 
+            id_user: userId,
+            token: passwordToken, 
+            password: password 
+        };
+
+        const response = await PasswordService.Update(submitPasswordData);
+
+        switch (response.status) {
+            case 200:
+                setIsLoading(false);
+                return alert('Nova senha cadastrada com sucesso');
+            case 400:
+            case 500:
+                setIsLoading(false);
+                reset();
+                return alert('E-mail e/ou senha inválido. Por favor, tente novamente.');
+        }
+    }
+    
     return(
-        <Styled.Form >
+        <Styled.Form onSubmit={ handleSubmit(submitForm) }>
         
             <Styled.Header>
                 <Styled.Title>Recuperar Senha</Styled.Title>
@@ -20,6 +55,12 @@ export const PasswordFormComponent = () => {
                     type='password'
                     placeholder='Digite sua senha'
                     label='Senha'
+                    register={{...register('password', { 
+                            required: true, 
+                            minLength: 8,
+                        })
+                    }}
+                    error={ errors.password }
                 />
 
                 <InputComponent
@@ -27,11 +68,19 @@ export const PasswordFormComponent = () => {
                     type='password'
                     placeholder='Digite sua senha'
                     label='Confirmar Senha'
+                    register={{...register('confirmPassword', { 
+                            required: true, 
+                            minLength: 8,
+                        })
+                    }}
+                    error={ errors.confirmPassword }
                 />
             </Styled.InputGroup>
 
-            <Styled.Button 
-                type='submit' 
+            <Styled.Button
+                $active={ !errors.confirmPassword && !errors.password }
+                type='submit'
+                disabled={ errors.confirmPassword || errors.password }
             > 
                 'Enviar' 
             </Styled.Button>
