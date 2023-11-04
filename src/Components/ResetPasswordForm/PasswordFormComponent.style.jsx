254ddd3e3b4e366174554375e126315a40dd0667
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-04 16:05:34 -0300

    added form to passwordFormComponent style

---
+++ src/Components/ResetPasswordForm/PasswordFormComponent.style.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+    display: inline-flex;
+    padding: 2rem;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 1rem;
+    border-radius: 0.625rem;
+    background: #FFF;
+    min-width: 320px;
+`;
