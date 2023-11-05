e30a0d08f735897868d888c0fdb43245905a168f
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-05 07:59:29 -0300

    added doctor name to cardConsulta

--- src/Components/CardConsulta/CardConsulta.jsx
+++ src/Components/CardConsulta/CardConsulta.jsx
@@ -1,12 +1,26 @@
 import * as Styled from './CardConsulta.style';
+import { useEffect, useState } from 'react';
+
+import { UserService } from '../../Service/User.service';
 
 export function CardConsulta ({ appointment })  {
-
+    const [usersList, setUsersList] = useState([]);
+    useEffect(() => {
+        UserService.Get().then(result => setUsersList(result))
+    }, [])
+    
+    const [filteredUser, setFilteredUser] = useState([]);
+    useEffect(() => {
+        if (usersList.length > 0) {
+            setFilteredUser(usersList.filter(value => value.id === appointment?.id_doctor));
+        }
+    }, [usersList])
+    
     return (
         <>
             <Styled.CardContainer>
 
-                <p>Médico: { } </p>
+                <p>Médico: { filteredUser[0]?.name } </p>
                 <p>Motivo da Consulta: {appointment?.appointment_reason} </p>
                 <p>Data da Consulta: {appointment?.appointment_date} </p>
                 <p>Horário da Consulta: {appointment?.appointment_hour} </p>
