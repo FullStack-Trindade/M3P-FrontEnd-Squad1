1acb59e5584778d5e715c0964f50064fee870542
gabrieldelpacheco <gabriel.del.pacheco@gmail.com>
2023-11-05 11:26:51 -0300

    colocado route de exercicio

--- App.jsx
+++ App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LoginPage } from "./src/Pages/LoginPage/LoginPage";
 import { Layout } from './src/Layout/Layout'
 import { CadastroExamPage } from './src/Pages/CadastroExame/CadastroExamPage';
+import { CadastroExercisePage } from './src/Pages/CadastroExercise/CadastroExercisePage.jsx';
 import { PacientePage } from "./src/Pages/CadastroPaciente/CadastroPaciente.jsx";
 import { CadastroUsuarioPage } from './src/Pages/CadastroUsuario/CadastroUsuario';
 
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' element={<Layout/>}>
               <Route path='/' element={<HomePage/>}/>
               <Route path='/exame' element={<CadastroExamPage/>}/>
+              <Route path='/exercise' element={<CadastroExercisePage/>}/>
           <Route path="/paciente" element={<PacientePage />} />
           <Route path="/paciente/:id" element={<PacientePage />} />
           <Route path="/listaProntuarios" element={<MedicalRecordsPage />} />
