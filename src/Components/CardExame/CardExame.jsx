b9c32c7540c577fdb9b7ce7080440b22257b031a
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-05 08:01:59 -0300

    added doctor name to cardExame

--- src/Components/CardExame/CardExame.jsx
+++ src/Components/CardExame/CardExame.jsx
@@ -1,12 +1,26 @@
 import * as Styled from './CardExame.style';
+import { useEffect, useState } from 'react';
+
+import { UserService } from '../../Service/User.service';
 
 export function CardExame ({ exam })  {
-
+    const [usersList, setUsersList] = useState([]);
+    useEffect(() => {
+        UserService.Get().then(result => setUsersList(result))
+    }, [])
+    
+    const [filteredUser, setFilteredUser] = useState([]);
+    useEffect(() => {
+        if (usersList.length > 0) {
+            setFilteredUser(usersList.filter(value => value.id === exam?.id_doctor));
+        }
+    }, [usersList])
+    
     return (
         <>
             <Styled.CardContainer>
 
-                <p>Médico: {  } </p>
+                <p>Médico: { filteredUser[0]?.name } </p>
                 <p>Nome do Exame: {exam?.nameExam} </p>
                 <p>Data do Exame: {exam?.dateExam} </p>
                 <p>Horário do Exame: {exam?.hourExam} </p>
