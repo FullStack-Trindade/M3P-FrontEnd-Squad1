29d7e1abe9b9316517f64661871ba306a4dc5dad
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-05 11:18:58 -0300

    added resetToken to logout button at MenuAberto

--- src/Components/Menu/MenuComponent/MenuAberto.jsx
+++ src/Components/Menu/MenuComponent/MenuAberto.jsx
@@ -1,7 +1,7 @@
 import * as Styled from "./MenuComponent.style";
 import MenuItem from "../MenuItem/MenuItem";
 import { ThemeContext } from '../../../Context/Theme.context.jsx'
-import { useContext} from 'react';
+import { useContext,  useEffect } from 'react';
 import {
   FaHome,
   FaSignInAlt,
@@ -9,15 +9,36 @@ import {
   FaTasks,
   FaLaptopMedical,
 } from "react-icons/fa";
-
 import { ImDroplet } from "react-icons/im";
 import { MdSoupKitchen } from 'react-icons/md';
 import { CiPill } from 'react-icons/ci';
 import { CgGym } from 'react-icons/cg';
 
+import { AuthService } from "../../../Service/Auth.service.jsx";
+import { AuthContext } from "../../../Context/auth.context.jsx";
+
 const MenuAberto = () => {
 
+  useEffect(() => { fetchAuth() }, []);
+  const localToken = JSON.parse(localStorage.getItem('token'));
+
+  const { idDatabase, setIdDatabase } = useContext(AuthContext);
+
+  const fetchAuth = async() => {
+    const authToken = await AuthService.Get();
+    const tokenExists = await authToken.filter(auth => auth.token_user === localToken);
+
+      if (tokenExists.length > 0) { 
+        setIdDatabase(tokenExists[0]?.id);
+      }
+    }
+
   const { theme } = useContext(ThemeContext);
+
+  const resetToken = () => {
+    localStorage.clear();
+    AuthService.Delete(idDatabase);
+  }
 
   return (
 
@@ -30,7 +51,12 @@ const MenuAberto = () => {
 
           <Styled.MenuSetor>Geral</Styled.MenuSetor>
           <MenuItem Icon={FaHome} Text="INICIO" To="/" />
-          <MenuItem Icon={FaSignInAlt} Text="SAIR" To="/login" />
+          <MenuItem 
+            Icon={FaSignInAlt} 
+            Text="SAIR" 
+            To="/login"
+            onClick={() => resetToken()}
+          />
 
           <Styled.MenuSetor>Pacientes</Styled.MenuSetor>
           <MenuItem Icon={FaPlus} Text="CADASTRAR PACIENTE" To="/paciente" />
