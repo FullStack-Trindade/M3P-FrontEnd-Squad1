3c53f21c9cdca42cfe24241f873afe6b713fd15c
gabrieldelpacheco <gabriel.del.pacheco@gmail.com>
2023-11-05 11:31:55 -0300

    correções em paciente service

--- src/Service/Paciente.service.jsx
+++ src/Service/Paciente.service.jsx
@@ -1,19 +1,21 @@
 const API_URL = `http://localhost:${import.meta.env.VITE_SERVER_PORT}/api/pacientes`
-const CadastrarPaciente = async (postPacientDb) => {
+
+const fetchUser = async (url, options) => {
   try {
     const response = await fetch(url, options);
-console.log(response);
-    if (!response.ok) {
-      throw new Error(
-        `Erro na requisição: ${response.status} - ${errorMessage.message}`
-      );
-    }
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Erro na chamada da API:", error);
+    throw error;
+  }
+};
 
-          const response = await fetch(API_URL, {
-
-export const PacienteService = {
+export const UserService = {
   Create: (data) => {
-    return fetchPatient(`${API_URL}/pacientes`, {
+    console.log(data)
+    return fetchUser(${API_URL}/usuarios, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -22,51 +24,28 @@ export const PacienteService = {
     });
   },
   Get: () => {
-    return fetchPatient(`${API_URL}/pacientes`);
+    return fetchUser(${API_URL}/usuarios);
   },
-  Show: (id) => {
-    return fetchPatient(`${API_URL}/pacientes/${id}`);
-  },
-  SearchByUserId: (id) => {
-    return fetchPatient(`${API_URL}/pacientes/usuario/${id}`);
+  
+  SearchByCpfEmail: (data) => {
+    return fetchUser(${API_URL}/usuarios/search, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
   },
   Update: (id, data) => {
-    return fetchPatient(`${API_URL}/pacientes/${id}`, {
+    return fetchUser(${API_URL}/usuarios/${id}, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     });
   },
   Delete: (id) => {
-    return fetchPatient(`${API_URL}/pacientes/${id}`, {
+    return fetchUser(${API_URL}/usuarios/${id}, {
       method: "DELETE",
-    });
-  },
+    });
+  },
 };
-
-
- const Get = (id) => {
-  const fetchPatient = async() => {
-      const response = await fetch(`API_URL/${id}`);
-      const data = await response.json();
-      return data;
-  }
-
-  return fetchPatient();
-}
- const GetAll = () => {
-  const fetchPatient = async() => {
-      const response = await fetch(API_URL);
-      const data = await response.json();
-      return data;
-  }
-
-  return fetchPatient();
-}
-
-export const PacienteService = {
-  CadastrarPaciente,
-  Get,
-  GetAll
-};
-
