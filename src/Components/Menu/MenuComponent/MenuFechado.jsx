775a2895600a9657d771192dbd42269ed78058f4
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-05 11:19:10 -0300

    added resetToken to logout button at MenuFechado

--- src/Components/Menu/MenuComponent/MenuFechado.jsx
+++ src/Components/Menu/MenuComponent/MenuFechado.jsx
@@ -1,6 +1,6 @@
 import * as Styled from './MenuComponent.style';
 import { ThemeContext } from '../../../Context/Theme.context.jsx'
-import { useContext} from 'react';
+import { useContext, useEffect } from 'react';
 import MenuItemFechado from '../MenuItemFechado/MenuItemFechado';
 import { 
 
@@ -16,11 +16,33 @@ import {
   import { CiPill } from 'react-icons/ci';
   import { CgGym } from 'react-icons/cg';
 
+import { AuthService } from '../../../Service/Auth.service.jsx';
+import { AuthContext } from '../../../Context/auth.context.jsx';
 
 
 const MenuFechado = () => {
+
+  useEffect(() => { fetchAuth() }, []);
+  const localToken = JSON.parse(localStorage.getItem('token'));
+
+  const { idDatabase, setIdDatabase } = useContext(AuthContext);
+
+  const fetchAuth = async() => {
+    const authToken = await AuthService.Get();
+    const tokenExists = await authToken.filter(auth => auth.token_user === localToken);
+
+      if (tokenExists.length > 0) { 
+        setIdDatabase(tokenExists[0]?.id);
+      }
+    }
+
+  const { theme } = useContext(ThemeContext);
+
+  const resetToken = () => {
+    localStorage.clear();
+    AuthService.Delete(idDatabase);
+  }
    
-    const { theme } = useContext(ThemeContext);
 
     return (
         
@@ -30,7 +52,11 @@ const MenuFechado = () => {
             <Styled.MenuLogoFechado src={'../../../public/images/LogoGenerica.png'} />
 
             <MenuItemFechado Icon={FaHome}  To='/' />
-            <MenuItemFechado Icon={FaSignInAlt}  To='/login' />
+            <MenuItemFechado 
+              Icon={FaSignInAlt}  
+              To='/login' 
+              onClick={() => resetToken()}
+            />
 
             <MenuItemFechado Icon={FaPlus}  To='/paciente' />
             <MenuItemFechado Icon={FaTasks}  To='/listaProntuarios'  />
