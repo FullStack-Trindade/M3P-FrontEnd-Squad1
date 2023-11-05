7caa3f10a5fb72d3a15a8cfb1f925f6cffcd9480
gabrieldelpacheco <gabriel.del.pacheco@gmail.com>
2023-11-05 11:29:30 -0300

    excluindo comentarios da exam service

--- src/Service/Exam.service.jsx
+++ src/Service/Exam.service.jsx
@@ -1,35 +1,3 @@
-// BUSCANDO NO DB DA API
-/* const CadastrarExame = async (postExameDb) => {
-    try {
-        const response = await fetch("http://localhost:3333/api/exames", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(postExameDb),
-        });
-        if (response.status === 201) {
-            const resultado = await response.json();
-            console.log(("Resposta do servidor:", resultado));
-            alert("Cadastro efetuado com sucesso");
-        } else {
-            const errorData = await response.json();
-            console.error("Erro ao cadastrar paciente. Status:", response.status);
-            console.error("Detalhes do erro:", errorData.message);
-            alert("Erro ao cadastrar paciente: " + errorData.message);
-        }
-    } catch (error) {
-        console.error("Error ao buscar dados:", error);
-        alert("Erro ao cadastrar paciente");
-    }
-};
-
-export const ExameService = {
-    CadastrarExame,
-}; */
-
-/* import { LocalStorageService } from "./LocalStorage.service"; */
-
 const API_URL = `http://localhost:${import.meta.env.VITE_SERVER_PORT}/api/exames`
 
 
