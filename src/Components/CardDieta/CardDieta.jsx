1eee9daabb3afc9e560d88576981febfc51e62cc
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-05 08:00:41 -0300

    added doctor name to cardDieta

--- src/Components/CardDieta/CardDieta.jsx
+++ src/Components/CardDieta/CardDieta.jsx
@@ -1,12 +1,26 @@
 import * as Styled from './CardDieta.style';
+import { useEffect, useState } from 'react';
+
+import { UserService } from '../../Service/User.service';
 
 export function CardDieta ({ diet })  {
-
+    const [usersList, setUsersList] = useState([]);
+    useEffect(() => {
+        UserService.Get().then(result => setUsersList(result))
+    }, [])
+    
+    const [filteredUser, setFilteredUser] = useState([]);
+    useEffect(() => {
+        if (usersList.length > 0) {
+            setFilteredUser(usersList.filter(value => value.id === diet?.id_doctor));
+        }
+    }, [usersList])
+    
     return (
         <>
             <Styled.CardContainer>
 
-                <p>Médico: {  } </p>
+                <p>Médico: { filteredUser[0]?.name } </p>
                 <p>Nome da Dieta: {diet?.diet_name} </p>
                 <p>Data da Dieta: {diet?.diet_date} </p>
                 <p>Horário da Dieta: {diet?.diet_hour} </p>
